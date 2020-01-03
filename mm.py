dfa35b1b62d219c7b7d0e45aedb3b995a337bc4a
Juan-Pablo Scaletti <juanpablo@jpscaletti.com>
2020-01-03 16:51:19 -0500

    Fix declaration of requirements versions so they don include the patch part.

--- mm.py
+++ mm.py
@@ -9,7 +9,7 @@ data = {
     "title": "Proper Form",
     "name": "proper_form",
     "pypi_name": "proper_form",
-    "version": "0.191105",
+    "version": "0.200103",
     "author": "Juan-Pablo Scaletti",
     "author_email": "juanpablo@jpscaletti.com",
     "description": "A not-terrible Python form library.",
@@ -24,10 +24,10 @@ data = {
     "development_status": "4 - Beta",
     "minimal_python": 3.6,
     "install_requires": [
-        "email-validator ~=1.0.4",
-        "idna ~=2.8",
-        "markupsafe ~=1.1.1",
-        "python-slugify ~=3.0.3",
+        "email-validator ~= 1.0",
+        "idna ~= 2.8",
+        "markupsafe ~= 1.1",
+        "python-slugify ~= 3.0",
     ],
     "testing_requires": [
         "pytest",
@@ -51,27 +51,27 @@ data = {
 }
 
 exclude = [
-    "copier.yml",
+    "hecto.yml",
     "README.md",
     ".git",
     ".git/*",
     ".venv",
     ".venv/*",
     ".DS_Store",
+    "CHANGELOG.md",
 ]
 
 
 def do_the_thing():
-    import copier
+    import hecto
 
-    copier.copy(
+    hecto.copy(
         # "gh:jpscaletti/mastermold.git",
         "../mastermold",  # Path to the local copy of Master Mold
         ".",
         data=data,
         exclude=exclude,
         force=False,
-        cleanup_on_error=False
     )
 
 
